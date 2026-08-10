4a9a35674b2e3bc4adbc91a5878d159ff419cb4e
Arapdavis01 <dancun6742@gmail.com>
2026-08-10 17:46:23 +0300

    Add project routes with CRUD operations

---
+++ backend/routes/projects.js
@@ -0,0 +1,13 @@
+const router = require('express').Router();
+const ctrl = require('../controllers/projectController');
+const auth = require('../middleware/authMiddleware');
+
+router.use(auth);
+
+router.get('/', ctrl.getAll);
+router.post('/', ctrl.create);
+router.get('/:id', ctrl.getOne);
+router.put('/:id', ctrl.update);
+router.delete('/:id', ctrl.remove);
+
+module.exports = router;
